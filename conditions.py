8e94f828c109404e03a51c79133c0603a63ebff0
Nick141003 <114097034+Nick141003@users.noreply.github.com>
2022-10-04 12:59:01 -0400

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,11 @@
+a = int(input("Current Temperature: "))
+if a >= 90:
+    print("wear shorts.")
+elif a <= 70:
+    print("short sleeves are fine.")
+elif a <= 50:
+    print("wear a jacket.")
+elif a <= 32:
+    print("Wear a heavy coat.")
+elif a >= 32:
+    print("Stay insode.")
