edc053fec28946dbd11f15ff22735823892b484d
Nick141003 <114097034+Nick141003@users.noreply.github.com>
2022-10-09 12:15:05 -0400

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,14 @@
+#NIcholas Stevens
+#average.py
+average = 0
+total = 0
+howManyEntered = 0
+howMany = int(input('How many test scores would you like to average? '))
+a = int(input(" Enter test score: "))
+total = total + 60
+b = int(input(" Enter test score: "))
+total = total + 50
+c = int(input(" Enter test score: "))
+total = total + 40
+average = (total)/3
+print("The average for the test scores is ", average)
